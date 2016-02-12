039dbad4f4a202ab4fa6e90f95b84491e30345d9
Kanitkorn S <k.sujautra@gmail.com>
2016-02-12 15:51:31 +0700

    Extract a function

--- n-gram/3-gram.py
+++ n-gram/3-gram.py
@@ -1,31 +1,29 @@
 import codecs
 
+def count_n_words(n):
+    if (len(words) >= n):
+        for i in range (n - 1, len(words) - 1):
+            key = words[i - 1] + ' _ ' + words[i]
+            if (n == 3):
+                key = words[i - 2] + ' _ ' + key
+            if (key in c[n - 2]):
+                c[n - 2][key] += 1
+            else:
+                c[n - 2][key] = 1
+
 file =  codecs.open('all_word_segmented_news.u8.txt', 'r', 'utf-8')
 
-c2 = {}
-c3 = {}
+c = [{},{}]
 
 for line in file:
     line = line.replace(' _ ', ' ')
     words = line.split(' ')
-    if (len(words) >= 2):
-        for i in range (1, len(words) - 1):
-            key = words[i - 1] + ' _ ' + words[i]
-            if (key in c2):
-                c2[key] += 1
-            else:
-                c2[key] = 1
-    if (len(words) >= 3):
-        for i in range (2, len(words) - 1):
-            key = words[i - 2] + ' _ ' + words[i - 1] + ' _ ' + words[i]
-            if (key in c3):
-                c3[key] += 1
-            else:
-                c3[key] = 1
+    count_n_words(2)
+    count_n_words(3)
 
 n = 0
-for c33 in c3:
+for c33 in c[1]:
     c33_split = c33.split(' _ ')
     c33_join = c33_split[0] + ' _ ' + c33_split[1]
     n += 1
-    print(str(n) + ' ' + c33 + ': ' + str(c3[c33] * 1.0 / c2[c33_join]))
+    print(str(n) + ' ' + c33 + ': ' + str(c[1][c33] * 1.0 / c[0][c33_join]))
